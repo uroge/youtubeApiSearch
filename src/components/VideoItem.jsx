05d9cd4c03b4e8672a8aa754aa997ed00af69d97
uroge <67282212+uroge@users.noreply.github.com>
2021-03-17 19:03:11 +0100

    created video-detail component and emmbeded videos on click to iframe

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
+    console.log(video.snippet);
     return (
-    <div className="video-item item">
-        <img src={video.snippet.thumbnails.medium.url} alt="" class="ui image"/>
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+        <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} className="ui image"/>
         <div className="content">
             <div className="header">{video.snippet.title}</div>    
         </div>
