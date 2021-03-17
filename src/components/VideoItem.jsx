99a0ad7a1bc3ac227f6a867c08c77798c08678fe
uroge <67282212+uroge@users.noreply.github.com>
2021-03-17 17:32:34 +0100

    displayed images from youtube api data in video-item component and added a little bit of style

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './VideoItem.css';
 
-const VideoItem = (props) => {
-    return <div>Video Item</div>
+const VideoItem = ({video}) => {
+    return (
+    <div className="video-item item">
+        <img src={video.snippet.thumbnails.medium.url} alt="" class="ui image"/>
+        <div className="content">
+            <div className="header">{video.snippet.title}</div>    
+        </div>
+    </div>);
 };
 
 export default VideoItem;
